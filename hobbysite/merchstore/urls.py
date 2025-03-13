b86d1d472d1eb095c0bdf811665710c4cc823bf9
Nathan Sapitula <natezzzzzzzz@users.noreply.github.com>
2025-03-13 20:16:37 +0800

    created views, fixed urls, initialized templates

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path("items/", itemList, name="itemList"),
+    path("item/<int:num>", itemEntry, name="itemEntry"),
 ]
--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+def itemList(request):
+    products = Product.objects.all()
+    product_types = ProductType.objects.all()
+    return render(request, "item_list.html")
+
+def itemEntry(request, num=1):
+
+    product = Product.objects.get(id=num)
+    product_type = product.product_type
+    return render(request, "item_entry.html")
