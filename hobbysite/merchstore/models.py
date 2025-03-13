97204b8c68faef72b22918ef2a96797bc45a9faa
Nathan Sapitula <natezzzzzzzz@users.noreply.github.com>
2025-03-13 20:03:46 +0800

    created models and admin pages

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class ProductType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return '{}'.format(self.name)
+    
+    def get_absolute_url(self):
+        return reverse('product_type', args=[str(self.id)])
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    product_type = models.ForeignKey(ProductType, on_delete=models.SET_NULL, related_name="products", null=True)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=100)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return '{}'.format(self.name)
+    
+    def get_absolute_url(self):
+        return reverse('product', args=[str(self.id)])
