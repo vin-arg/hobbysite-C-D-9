0b715f7c92b7433fb3e0fc064f5800339f700196
vin-arg <vinceashanti@gmail.com>
2025-03-13 21:24:17 +0800

    fixed views, added code to models and admin, and finished html templates

--- hobbysite/blog/admin.py
+++ hobbysite/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import ArticleCategory, Article
 
-# Register your models here.
+class ArticleCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description',)
+    search_fields = ('name',)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'created_on', 'updated_on',)
+    search_fields = ('title', 'entry',)
+    list_filter = ('category', 'created_on',)
+
+admin.site.register(ArticleCategory, ArticleCategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
