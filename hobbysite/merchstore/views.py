d9c1a5d25a599e3504a9092249882405fd008cc4
Nathan Sapitula <natezzzzzzzz@users.noreply.github.com>
2025-03-13 20:52:25 +0800

    finished html templates, fixed project urls

--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -4,10 +4,10 @@ from .models import *
 def itemList(request):
     products = Product.objects.all()
     product_types = ProductType.objects.all()
-    return render(request, "item_list.html")
+    return render(request, "item_list.html", {'products': products, 'product_types': product_types})
 
 def itemEntry(request, num=1):
 
     product = Product.objects.get(id=num)
     product_type = product.product_type
-    return render(request, "item_entry.html")
+    return render(request, "item_entry.html", {'product': product, 'product_type': product_type})
