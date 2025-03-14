e6912ea3fc3365b1d4cd59743a7b1d59263002c4
David Tormon <davidtormon@gmail.com>
2025-03-14 19:41:20 +0800

    forgot comma before parens

--- hobbysite/commissions/admin.py
+++ hobbysite/commissions/admin.py
@@ -3,13 +3,13 @@ from .models import Commission, Comment
 
 class CommissionAdmin(admin.ModelAdmin):
     model = Commission
-    list_display = ('title', 'description', 'people_required', 'created_on')
+    list_display = ('title', 'description', 'people_required', 'created_on',)
     search_fields = ('title',)
 
 
 class CommentAdmin(admin.ModelAdmin):
     model = Comment
-    list_display = ('created_on', 'updated_on')
+    list_display = ('created_on', 'updated_on',)
     search_fields = ('title', 'entry',)
 
 admin.site.register(Commission, CommissionAdmin)
