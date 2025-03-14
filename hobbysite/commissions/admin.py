2cdd5b4e7a04bf4c074cd0c70770facaaefaa84a
David Tormon <davidtormon@gmail.com>
2025-03-14 20:52:24 +0800

    removed unnecessary imports

--- hobbysite/commissions/admin.py
+++ hobbysite/commissions/admin.py
@@ -4,13 +4,14 @@ from .models import Commission, Comment
 class CommissionAdmin(admin.ModelAdmin):
     model = Commission
     list_display = ('title', 'description', 'people_required', 'created_on',)
+    list_filter = ('title',)
     search_fields = ('title',)
 
 
 class CommentAdmin(admin.ModelAdmin):
     model = Comment
-    list_display = ('created_on', 'updated_on',)
-    search_fields = ('title', 'entry',)
+    list_display = ('commission', 'created_on', 'updated_on',)
+    search_fields = ('commission', 'title', 'entry',)
 
 admin.site.register(Commission, CommissionAdmin)
 admin.site.register(Comment, CommentAdmin)
--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from .models import Comment, Commission
+from .models import Commission
 
 def commissions_list(request):
     commissions = Commission.objects.all()
     return render(request, "commission_content.html", {'commissions': commissions},)
 
 def commissions_detail(request, pk):
-    details = Commission.objects.get(pk=pk)
-    return render(request, "commission_comments.html", {"details": details},)
+    comments = Commission.objects.get(pk=pk)
+    return render(request, "commission_comments.html", {"comments": comments},)
