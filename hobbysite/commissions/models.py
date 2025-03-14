f3fb89a5518e21acd263d8246a9a9e43ec4d4132
David Tormon <davidtormon@gmail.com>
2025-03-14 15:11:47 +0800

    modified created_on and updated_on to work properly

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    people_required = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('ledger:ingredientDetail', args=[self.pk])
+    
+class Comment(models.Model):
+    commission = models.ForeignKey(Commission, on_delete=models.CASCADE)
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('ledger:recipeDetail', args=[self.pk])
