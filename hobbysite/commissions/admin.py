202b7e7366cf4336a1c66f22769549f23a027e49
David Tormon <davidtormon@gmail.com>
2025-03-14 19:09:02 +0800

    assigned models to admin classes

--- hobbysite/commissions/admin.py
+++ hobbysite/commissions/admin.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from .models import Commission, Comment
 
 class CommissionAdmin(admin.ModelAdmin):
+    model = Commission
     list_display = ('title', 'description', 'people_required', 'created_on')
     search_fields = ('title',)
 
 
 class CommentAdmin(admin.ModelAdmin):
+    model = Comment
     list_display = ('created_on', 'updated_on')
     search_fields = ('title', 'entry',)
     list_filter = ('people_required',)
