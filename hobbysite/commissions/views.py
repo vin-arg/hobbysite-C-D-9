0504a033cc1908ff10ad9a243a83c74a85a2b69e
David Tormon <davidtormon@gmail.com>
2025-03-14 17:11:37 +0800

    added to views.py to reflect used models

--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+from .models import *
+
+def commissions_list(request):
+    commissions = Commission.objects.all()
+    return render(request, "item_list.html", {'commissions': commissions})
+
+def commissions_detail(request, num=1):
+    details = Comment.objects.get(id=num)
+    return render(request, "item_entry.html", {"details": details})
