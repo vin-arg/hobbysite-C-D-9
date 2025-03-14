5f88044dcba2a81cd62275edbc8f23da41e92271
Nathan Sapitula <natezzzzzzzz@users.noreply.github.com>
2025-03-14 23:48:14 +0800

    initial final commit

--- hobbysite/forum/models.py
+++ hobbysite/forum/models.py
@@ -6,6 +6,9 @@ class PostCategory(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
     #sort by name
     class Meta:
         ordering = ['name']
