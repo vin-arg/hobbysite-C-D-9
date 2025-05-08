ae24dbeb0123795d61c0006e7875c998f3490874
Nathan Sapitula <teetan888@gmail.com>
2025-05-08 18:46:16 +0800

    modified settings, added models and admin pages, added login and home page
    consider centralizing css for consistency, RENAME files also for better documentation

--- hobbysite/user_management/models.py
+++ hobbysite/user_management/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=63, default="")
+    email = models.EmailField(max_length=255)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+    
+    def get_absolute_url(self):
+        return reverse('profile', args=[str(self.id)])
