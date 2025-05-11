878bac180187d4d490ff2eb19d551dd09f501b48
David Tormon <davidtormon@gmail.com>
2025-05-11 09:00:27 +0800

    added all paths

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -6,4 +6,6 @@ app_name = "commissions"
 urlpatterns = [
     path('list/', views.commissions_list, name = 'commissions_list'),
     path('detail/<int:pk>', views.commissions_detail, name = 'commissions_detail'),
+    path('add/', views.commissions_list, name = 'commissions_add'),
+    path('<int:pk>/edit', views.commissions_detail, name = 'commissions_edit'),
 ]
