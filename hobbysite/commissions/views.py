7d6b6c6960343a4ef6ee879e5d0ff9152423aa2e
David Tormon <davidtormon@gmail.com>
2025-05-12 06:37:05 +0800

    formatted views to ctx

--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -3,8 +3,15 @@ from .models import Commission
 
 def commissions_list(request):
     commissions = Commission.objects.all()
-    return render(request, 'commission_content.html', {'commissions': commissions},)
+    ctx = {
+        'commissions': commissions
+    }
+    return render(request, 'commission_content.html', ctx,)
 
 def commissions_detail(request, pk):
     commission_info = Commission.objects.get(pk=pk)
-    return render(request, 'commission_comments.html', {'comments': commission_info.comments.all(), 'commission': commission_info},)
+    ctx = {
+        'comments': commission_info.comments.all(),
+        'commission': commission_info
+    }
+    return render(request, 'commission_comments.html', ctx,)
