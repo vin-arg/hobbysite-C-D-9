343b7d237f407f0229196d829295b223e2c05a4a
David Tormon <davidtormon@gmail.com>
2025-05-12 15:08:52 +0800

    added modeladmins for the necessary models

--- hobbysite/commissions/admin.py
+++ hobbysite/commissions/admin.py
@@ -1,17 +1,20 @@
 from django.contrib import admin
-from .models import Commission, Comment
+from .models import Commission, Job, JobApplication
+
+class JobInLine(admin.TabularInline):
+    """Inline admin for jobs"""
+    model = Job
 
 class CommissionAdmin(admin.ModelAdmin):
     model = Commission
-    list_display = ('title', 'description', 'people_required', 'created_on',)
-    list_filter = ('title',)
-    search_fields = ('title',)
+    inlines = [JobInLine]
 
+class JobAdmin(admin.ModelAdmin):
+    model = Job
 
-class CommentAdmin(admin.ModelAdmin):
-    model = Comment
-    list_display = ('commission', 'created_on', 'updated_on',)
-    search_fields = ('commission', 'title', 'entry',)
+class JobApplicationAdmin(admin.ModelAdmin):
+    model = JobApplication
 
 admin.site.register(Commission, CommissionAdmin)
-admin.site.register(Comment, CommentAdmin)
+admin.site.register(Job, JobAdmin)
+admin.site.register(JobApplication, JobApplicationAdmin)
