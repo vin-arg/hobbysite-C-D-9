cb6cfac8be9c31f0a8e29cca8c070f951c47313b
Nathan Sapitula <natezzzzzzzz@users.noreply.github.com>
2025-05-12 17:24:22 +0800

    added registration page

--- hobbysite/user_management/views.py
+++ hobbysite/user_management/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .models import Profile
+from .forms import RegisterForm
 
 def home_page(request):
     return render(request, 'home.html')
+
+def register(request):
+    form = RegisterForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            Profile.objects.create(user=user, name=user.username, email=user.email)
+            login(request, user)
+            return redirect('home_page')   
+    return render(request, 'registration/register.html', {'regform': form})
