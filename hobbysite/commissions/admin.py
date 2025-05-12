81043763f798368da90212a8d764a929846f3498
DavidTMN <davidtormon@gmail.com>
2025-05-12 18:21:00 +0800

    removed unneccessarry code in admin.py

--- hobbysite/commissions/admin.py
+++ hobbysite/commissions/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import Commission, Job, JobApplication
 
 class JobInLine(admin.TabularInline):
-    """Inline admin for jobs"""
     model = Job
 
 class CommissionAdmin(admin.ModelAdmin):
